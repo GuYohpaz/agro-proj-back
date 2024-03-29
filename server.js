92ea16c8455ff74c66b28034660c7900742f838a
guy yohpaz <guyos3532@gmail.com>
2024-03-30 00:33:53 +0300

    heroku08

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.use('/api/shape', shapeRoutes)
 app.use('/api/seedling', seedlingRoutes)
 app.use('/api/bag', bagRoutes)
 
+
+const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
+app.all('*', setupAsyncLocalStorage)
+
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/car/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
